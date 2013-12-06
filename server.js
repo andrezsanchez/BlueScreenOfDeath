7b1fb6791b58e4361c43733032717d22ddcf21d7
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2013-12-06 01:03:22 -0500

    Use correct directory for static files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ function createApp() {
   var app = express()
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade')
-  app.use(express.static('app'))
+  app.use(express.static(__dirname + '/app'))
 
   app.get('/', function (req, res) {
     res.render('index.jade')
